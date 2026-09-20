498f9d4d6b2689cc8936ef028e94fb1183df10f3
EmadAdel22 <emadadelgabar@gmail.com>
2026-09-20 20:56:58 +0300

    Use Swagger

--- jobApplication.Api/Program.cs
+++ jobApplication.Api/Program.cs
@@ -74,15 +74,18 @@ namespace jobApplication.Api
 
             builder.Services.AddAuthorization();
 
-
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
-                app.MapScalarApiReference();
+                //app.MapOpenApi();
+                //app.MapScalarApiReference();
+                app.UseSwagger();
+                app.UseSwaggerUI();
 
             }
 
